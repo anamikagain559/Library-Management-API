25957f400a79fe326e04fd6bde6b159afa83cf58
anamikagain559 <anamika.zaimahtech@gmail.com>
2025-06-23 16:38:45 +0600

    note model added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,37 @@
-
-import express,{Application, Request, Response} from 'express';
+import express,{Application, Request, Response,} from 'express';
+import { Schema,model } from 'mongoose';  
 const app:Application = express()
-const port = 3000
+app.use(express.json());
+const noteSchema = new Schema({
+  title: String, 
+  content: String,
+ });
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
-})
+const Note = model('Note', noteSchema);
 
+// Create Note route
+app.post('/create-note', async (req: Request, res: Response) => {
+  try {
+    const myNote = new Note({
+      title: 'My First Note',
+      content: 'This is the content of my first note.',
+    });
 
+    await myNote.save(); // Save to MongoDB
 
+    res.status(201).json({
+      success: true,
+      message: 'Note created successfully',
+      note: myNote,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Failed to create note', error });
+  }
+});
+
+// Home route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
 
 export default app;
--- src/app/server.ts
+++ src/app/server.ts
@@ -1,13 +1,22 @@
+import { Server } from 'http';
 import app from './app';
-let server ;
+import mongoose from 'mongoose';
+
+let server: Server;
 const port = 5000;
 
+async function main() {
+  try {
 
-const bootstrap = async () => {
-
-  server = app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    await mongoose.connect('mongodb+srv://anamikagain8:1xvOREUhSh2qWGyq@cluster0.o6amai6.mongodb.net/MyNoteDB?retryWrites=true&w=majority&appName=Cluster0');
+    console.log('Connected to MongoDB');
+      server = app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.error('Error starting the server:', error);
+  }
 }
 
-bootstrap()
+main();
+
