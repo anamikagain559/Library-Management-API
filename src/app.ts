e7b2cad89d887f357a25bd87f5f5578b1d2e8085
anamikagain559 <anamika.zaimahtech@gmail.com>
2025-07-18 20:52:38 +0600

    cors added

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express, { Application } from 'express';
 import { errorHandler } from './middlewares/errorHandler';
 import { bookRoutes } from './app/routes/book.route';
 import { borrowRoutes } from './app/routes/borrow.route';
+import cors from "cors";
 const app: Application = express();
 
 app.use(express.json());
+app.use(cors({origin: '*'}));
 app.get('/', (req, res) => {
   res.send('Welcome to the API library.');
 });
